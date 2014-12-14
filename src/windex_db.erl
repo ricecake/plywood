9109be3adf71c365209f9fdd955a7c55bfcf8bb6
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-14 17:52:52 -0500

    basic db manager module

---
+++ src/windex_db.erl
@@ -0,0 +1,15 @@
+-module(windex_db).
+
+-export([open/1, open/2, close/1, store/3, fetch/2]).
+
+open(Name) when is_atom(Name) -> open(Name, "./").
+open(Name, Base) when is_atom(Name), is_list(Base) ->
+        File = lists:append([Base, atom_to_list(Name), ".hanoidb"]),
+        hanoidb:open_link({local, Name}, File, [{compress, snappy}]).
+
+close(Db) -> hanoidb:close(Db).
+
+store(Db, Key, Value) -> hanoidb:put(Db, term_to_binary(Key), term_to_binary(Value)).
+fetch(Db, Key)        ->
+        {ok, BinaryTerm} = hanoidb:get(Db, term_to_binary(Key)),
+        {ok, binary_to_term(BinaryTerm)}.
