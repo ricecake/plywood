1fb6f206ffda8a7e21169191b27d2973bfa0f87e
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-14 17:01:19 -0500

    initial commit -- generated files and configs

---
+++ src/windex_app.erl
@@ -0,0 +1,16 @@
+-module(windex_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    windex_sup:start_link().
+
+stop(_State) ->
+    ok.
