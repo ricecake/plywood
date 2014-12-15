285e04190e31cddd28d0ae044f95df9fbcf3f3c6
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-14 20:16:35 -0500

    cleanup lookup and add logic a bit

--- src/windex.erl
+++ src/windex.erl
@@ -1,7 +1,7 @@
 -module(windex).
 
 -export([lookup/2, add/2, delete/2]).
-
+-compile(export_all).
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -11,7 +11,11 @@ lookup(Index, Key) ->
         {ok, RootNode} = windex_db:fetch(primary_tree, Index),
         doLookup(KeyParts, RootNode).
 
-add(Index, Rev) when is_map(Rev) -> ok.
+add(Index, Rev) when is_map(Rev) ->
+        RevTree = makeTree(Rev),
+        case windex_db:getIfExists(primary_tree, Index) of
+                false -> windex_db:store(primary_tree, Index, RevTree)
+        end.
 
 delete(Index, Rev) -> ok.
 
@@ -21,6 +25,13 @@ delete(Index, Rev) -> ok.
 
 doLookup([<<>> | Rest], Node) -> doLookup(Rest, Node);
 doLookup([], Node) -> Node;
-doLookup([Label | Rest], Node) when is_map(Node) ->
+doLookup([Label | Rest], {Node, _Data}) when is_map(Node) ->
         NewNode = maps:get(Label, Node),
         doLookup(Rest, NewNode).
+
+convertNode(Value) when is_map(Value) -> {doMakeTree(Value), undefined};
+convertNode(Value) when is_list(Value) -> {undefined, Value}.
+
+makeTree(Revision) when is_map(Revision) -> {doMakeTree(Revision), undefined}.
+doMakeTree(Revision) when is_map(Revision) ->
+        maps:map(fun(_Key, Value) -> convertNode(Value) end, Revision).
--- src/windex_db.erl
+++ src/windex_db.erl
@@ -1,6 +1,6 @@
 -module(windex_db).
 
--export([open/1, open/2, close/1, store/3, fetch/2, exists/2]).
+-export([open/1, open/2, close/1, store/3, fetch/2, getIfExists/2]).
 
 open(Name) when is_atom(Name) -> open(Name, "./").
 open(Name, Base) when is_atom(Name), is_list(Base) ->
@@ -16,8 +16,8 @@ fetch(Db, Key)        ->
                 not_found        -> throw({not_found, Key})
         end.
 
-exists(Db, Key) ->
+getIfExists(Db, Key) ->
         case hanoidb:get(Db, term_to_binary(Key)) of
-                {ok, BinaryTerm} -> true;
-                not_found        -> false
+                {ok, BinaryTerm} -> {ok, binary_to_term(BinaryTerm)};
+                not_found         -> false
         end.
