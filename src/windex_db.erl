cb584257be45fd0ee1e528a61382987bc087bdd9
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-14 19:18:39 -0500

    add some functionality to db

--- src/windex_db.erl
+++ src/windex_db.erl
@@ -1,6 +1,6 @@
 -module(windex_db).
 
--export([open/1, open/2, close/1, store/3, fetch/2]).
+-export([open/1, open/2, close/1, store/3, fetch/2, exists/2]).
 
 open(Name) when is_atom(Name) -> open(Name, "./").
 open(Name, Base) when is_atom(Name), is_list(Base) ->
@@ -11,5 +11,13 @@ close(Db) -> hanoidb:close(Db).
 
 store(Db, Key, Value) -> hanoidb:put(Db, term_to_binary(Key), term_to_binary(Value)).
 fetch(Db, Key)        ->
-        {ok, BinaryTerm} = hanoidb:get(Db, term_to_binary(Key)),
-        {ok, binary_to_term(BinaryTerm)}.
+        case hanoidb:get(Db, term_to_binary(Key)) of
+                {ok, BinaryTerm} -> {ok, binary_to_term(BinaryTerm)};
+                not_found        -> throw({not_found, Key})
+        end.
+
+exists(Db, Key) ->
+        case hanoidb:get(Db, term_to_binary(Key)) of
+                {ok, BinaryTerm} -> true;
+                not_found        -> false
+        end.
