cc2239c6e72606159a206cbb544f3da2fb60841b
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-14 21:08:41 -0500

    git basic tree merge functionality running

--- src/windex.erl
+++ src/windex.erl
@@ -13,9 +13,11 @@ lookup(Index, Key) ->
 
 add(Index, Rev) when is_map(Rev) ->
         RevTree = makeTree(Rev),
-        case windex_db:getIfExists(primary_tree, Index) of
-                false -> windex_db:store(primary_tree, Index, RevTree)
-        end.
+        NewTree = case windex_db:getIfExists(primary_tree, Index) of
+                false -> RevTree;
+                {ok, FullTree} -> mergeTrees(FullTree, RevTree)
+        end,
+        windex_db:store(primary_tree, Index, NewTree).
 
 delete(Index, Rev) -> ok.
 
@@ -29,9 +31,22 @@ doLookup([Label | Rest], {Node, _Data}) when is_map(Node) ->
         NewNode = maps:get(Label, Node),
         doLookup(Rest, NewNode).
 
-convertNode(Value) when is_map(Value) -> {doMakeTree(Value), undefined};
-convertNode(Value) when is_list(Value) -> {undefined, Value}.
+convertNode(Value) when is_map(Value) -> {doMakeTree(Value), []};
+convertNode(Value) when is_list(Value) -> {#{}, Value}.
 
-makeTree(Revision) when is_map(Revision) -> {doMakeTree(Revision), undefined}.
+makeTree(Revision) when is_map(Revision) -> {doMakeTree(Revision), []}.
 doMakeTree(Revision) when is_map(Revision) ->
         maps:map(fun(_Key, Value) -> convertNode(Value) end, Revision).
+
+mergeTrees({LsubTree, Ldata}, {RsubTree, Rdata}) ->
+        NewData = lists:append(Ldata, Rdata),
+        {hashMerge(LsubTree, RsubTree), NewData}.
+
+hashMerge(Left, Right) when is_map(Left), is_map(Right) -> hashMerge(Left, maps:to_list(Right));
+hashMerge(Left, [])    when is_map(Left) -> Left;
+hashMerge(Left, [{Key, Value} |Rest]) when is_map(Left) ->
+        NewValue = case maps:find(Key, Left) of
+                error -> Value;
+                {ok, LeftValue} -> mergeTrees(LeftValue, Value)
+        end,
+        hashMerge(maps:put(Key, NewValue, Left), Rest).
