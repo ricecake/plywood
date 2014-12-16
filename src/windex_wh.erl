000e12d5f20d33a6ebeb1b3b578526b2a94d5050
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-12-16 14:50:57 -0500

    cleanup of webhandler code

--- src/windex_wh.erl
+++ src/windex_wh.erl
@@ -2,57 +2,38 @@
 
 %% Standard callbacks.
 -export([init/2]).
--export([allowed_methods/2]).
--export([content_types_provided/2]).
--export([content_types_accepted/2]).
--export([resource_exists/2]).
 
--export([lookup/2, insert/2]).
 
 init(Req, Opts) ->
-	{cowboy_rest, Req, Opts}.
+	Method = cowboy_req:method(Req),
+	HasBody = cowboy_req:has_body(Req),
+	Req2 = process(Method, HasBody, Req),
+	{ok, Req2, Opts}.
 
-allowed_methods(Req, State) ->
-	{[<<"GET">>, <<"POST">>], Req, State}.
-
-content_types_provided(Req, State) ->
-	{[
-		{{<<"text">>, <<"json">>, []}, lookup}
-	], Req, State}.
-
-content_types_accepted(Req, State) ->
-	{[
-                {{<<"text">>, <<"json">>, []}, insert}
-        ], Req, State}.
-
-resource_exists(Req, _State) ->
-	case cowboy_req:binding(index, Req) of
-		undefined -> {true, Req, index};
-		Index ->
-			case windex_db:exists(primary_tree, Index) of
-				true  -> {true, Req, Index};
-				false -> {false, Req, Index}
-			end
-	end.
-
-insert(Req, State) ->
+process(<<"GET">>, _Body, Req) ->
+	Index = cowboy_req:binding(index, Req),
+	Path = cowboy_req:path_info(Req),
+	lookup(Index, Path, Req);
+process(<<"PUT">>, true, Req) ->
 	Index = cowboy_req:binding(index, Req),
 	{ok, Data, Req2} = cowboy_req:body(Req),
-	ok = windex:add(Index, jiffy:decode(Data, [return_maps])),
-	case cowboy_req:method(Req2) of
-		<<"POST">> ->
-			{{true, <<"/t/", Index/binary>>}, Req2, State};
-		_ ->
-			{true, Req2, State}
-	end.
+	insert(Index, Data, Req2);
+process(<<"PUT">>, false, Req) ->
+	cowboy_req:reply(400, [], <<"Missing body.">>, Req);
+process(_, _, Req) ->
+	%% Method not allowed.
+	cowboy_req:reply(405, Req).
 
-lookup(Req, Index) ->
-        io:format("~p~n", [{Req, Index, cowboy_req:path_info(Req)}]),
-        Path = cowboy_req:path_info(Req),
+lookup(Index, Path, Req) ->
+
         try windex:lookup(Index, Path) of
                 Data ->
                         Result = windex:export(Data),
-                        { jiffy:encode(Result), Req, Index }
+                        cowboy_req:reply(200, [], jiffy:encode(Result), Req)
         catch
-                Exception:Reason -> {"Null", Req, Index}
+                Exception:Reason -> cowboy_req:reply(500, [], <<"Error">>, Req)
         end.
+
+insert(Index, Data, Req) ->
+	ok = windex:add(Index, jiffy:decode(Data, [return_maps])),
+	cowboy_req:reply(200, Req).
