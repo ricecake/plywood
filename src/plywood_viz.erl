98b981baf1e713e9d1e7f44c4cffa1398eafe672
Sebastian Green-Husted <ricecake@tfm.nu>
2014-12-27 21:46:14 -0500

    fix a pattern match in viz handler

--- src/plywood_viz.erl
+++ src/plywood_viz.erl
@@ -5,5 +5,5 @@
 init(Req, Opts) ->
 	Index = cowboy_req:binding(index, Req),
 	{ok, Body} = treeview_dtl:render([{index, Index}]),
-	{ok, Reply} = cowboy_req:reply(200, [], Body, Req),
-	{ok, Reply, Opts}.
+	Req2 = cowboy_req:reply(200, [], Body, Req),
+	{ok, Req2, Opts}.
