14a0537d707c8e1c7c1ee7704895ec3244971d40
Shane Utt <shaneutt@gmail.com>
2015-02-13 06:17:57 -0500

    whitespace

--- src/plywood_app.erl
+++ src/plywood_app.erl
@@ -10,23 +10,23 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    {ok, Pid} = plywood_sup:start_link(),
-    Dispatch = cowboy_router:compile([
+	{ok, Pid} = plywood_sup:start_link(),
+	Dispatch  = cowboy_router:compile([
 	    {'_', [
-			{"/tree/:index/[...]", plywood_wh, []},
-			{"/view/:index", plywood_viz, []},
-			{"/mutate/:index", plywood_modify, []},
-			{"/[...]",         cowboy_static, {priv_dir, plywood, "html/"}},
-			{"/js/[...]",      cowboy_static, {priv_dir, plywood, "js/"  }},
-			{"/css/[...]",     cowboy_static, {priv_dir, plywood, "css/" }}
+			{"/tree/:index/[...]", plywood_wh,     []},
+			{"/view/:index",       plywood_viz,    []},
+			{"/mutate/:index",     plywood_modify, []},
+			{"/[...]",             cowboy_static,  {priv_dir, plywood, "html/"}},
+			{"/js/[...]",          cowboy_static,  {priv_dir, plywood, "js/"  }},
+			{"/css/[...]",         cowboy_static,  {priv_dir, plywood, "css/" }}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 25, [{port, 8080}],
-        				[
+					[
 						{compress, true},
 						{env, [{dispatch, Dispatch}]}
 					]),
 	{ok, Pid}.
 
 stop(_State) ->
-    ok.
+	ok.
